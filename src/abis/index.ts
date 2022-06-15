fe04aa35f5f93d145d7ae3e8ccd4d5eb75da909b
Bowen Xue <xboxue@gmail.com>
2022-06-15 02:46:47 -0400

    Export ABIs

---
+++ src/abis/index.ts
@@ -0,0 +1,6 @@
+import erc20Abi from "./erc20.abi.json";
+import { factoryAbi } from "./LaserProxyFactory.json";
+import { abi as walletAbi } from "./LaserWallet.json";
+import { entryPointAbi } from "./TestEntryPoint.json";
+
+export { erc20Abi, factoryAbi, walletAbi, entryPointAbi };
